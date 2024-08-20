93a7db0a7637d49744ebcd32f8b184df00e4962a
ketutk <ketutkrisnakertajaya@gmail.com>
2024-08-20 21:10:14 +0700

    init2

--- api/index.js
+++ api/index.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
